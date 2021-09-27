086c1b8ab5c17936d2cbd42a4ea214a07178114d
Normx1 <74204452+Normx1@users.noreply.github.com>
2021-09-27 17:37:25 +0300

    fix  of interpolation

--- src/Punct1.java
+++ src/Punct1.java
@@ -4,7 +4,7 @@ public class Punct1 {
     double Mmax;
 
     static double degr(double a, int b) {
-        for (int i = b; i > 1 ; i--) {
+        for (int i = b; i > 1; i--) {
             a = a * a;
         }
         return a;
@@ -33,10 +33,18 @@ public class Punct1 {
 
     static double hmin(double R, double l, double q1N, double q2, double f) {
         double Hmin = 5 / 24 * (R * Yc / Yn) *
-                (l / (2.06 * degr(10,5)))*(f/1)*(q1N/q2);
+                (l / (2.06 * degr(10, 5))) * (f / 1) * (q1N / q2);
         return Hmin;
+    }
 
+    static double interpolation(double firstVal, double secondVal,
+                                double firstCount, double secCount,
+                                double myVal) {
+        return (firstCount + ((secCount - firstCount) / (secondVal - firstVal)) * (myVal - firstVal));
 
     }
 
+    public static void main(String[] args) {
+        System.out.println(interpolation(6, 24, 200, 250, 13));
+    }
 }
