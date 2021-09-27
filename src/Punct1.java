29bbb932df8977965af25caef17744e7f5557515
Normx1 <74204452+Normx1@users.noreply.github.com>
2021-09-27 16:47:32 +0300

    1st

---
+++ src/Punct1.java
@@ -0,0 +1,42 @@
+public class Punct1 {
+    static double Yn = 0.95;
+    static double Yc = 1.1;
+    double Mmax;
+
+    static double degr(double a, int b) {
+        for (int i = b; i > 1 ; i--) {
+            a = a * a;
+        }
+        return a;
+    }
+
+    static double Moment(double q, double l) {
+        double Mmax = (q * (l * l)) / 8;
+        return Mmax;
+    }
+
+    static double Qsila(double q, double l) {
+        double Qmax = (q * l) / 2;
+        return Qmax;
+    }
+
+    static double GnormNapr(double Mmax, double c, double W) {
+        double Gnorm = Mmax * Yn / (c * W * Yc);
+        return Gnorm;
+    }
+
+    static double W(double Mmax, double c, double R) {
+
+        double Wn = Mmax * Yn / (R * c * Yc);
+        return Wn;
+    }
+
+    static double hmin(double R, double l, double q1N, double q2, double f) {
+        double Hmin = 5 / 24 * (R * Yc / Yn) *
+                (l / (2.06 * degr(10,5)))*(f/1)*(q1N/q2);
+        return Hmin;
+
+
+    }
+
+}
